20b5052b1ccaaef6b8729c916b3f1af90cc20f56
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-10-08 19:53:14 +0200

    GrekiskaWebViewApp

---
+++ app/src/main/java/se/grekiska/logicash/MainActivity.java
@@ -0,0 +1,46 @@
+package se.grekiska.logicash;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://grekiska.se " );
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);  // Enable JavaScript if needed
+        webSettings.setDomStorageEnabled(true);  // Enable DOM storage if needed
+        webSettings.setUseWideViewPort(true);
+
+        webSettings.setAllowContentAccess(true);
+        webSettings.setAllowFileAccess(true);
+
+        webSettings.setDisplayZoomControls(true);
+        webSettings.setSupportZoom(true);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
